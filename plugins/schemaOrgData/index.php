4b083b5214d806b6cac413b18e97a72973b78d0e
bernhardunger <github@bernhardunger.de>
2026-07-10 13:24:04 +0200

    Version: PLUGIN_VERSION auf 0.9.7-rc nachgezogen
    PLUGIN_VERSION war seit Commit cbe72f3 (0.9.5-rc) über zwei
    Produktivcode-Commits (3ae1b86, 73cbdce) unverändert geblieben,
    obwohl jeder davon laut Konvention einen Patch-Inkrement ausgelöst
    hätte. Zwei Inkremente in diesem Commit nachgeholt. Keine
    funktionale Änderung.

--- plugins/schemaOrgData/index.php
+++ plugins/schemaOrgData/index.php
@@ -50,7 +50,7 @@ require_once __DIR__.'/lib/SchemaOrgData_AdminRequestContext.php';
 class schemaOrgData extends Plugin {
 
     /** Plugin-Version, siehe getInfo() */
-    private const PLUGIN_VERSION = '0.9.5-rc';
+    private const PLUGIN_VERSION = '0.9.7-rc';
 
     /** Standard-Sprache, falls die CMS-/Admin-Sprache nicht unterstützt wird */
     private const DEFAULT_LANGUAGE = 'deDE';
